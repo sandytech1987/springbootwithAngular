bf09fc88ecf84ac48a0deb2d08f7af4014e807ef
sandytech1987 <61221856+sandytech1987@users.noreply.github.com>
2020-03-26 16:01:34 -0600

    commit chages databases

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,7 @@ package com.luv2code.ecommerce.dao;
 import com.luv2code.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
